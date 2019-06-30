a55337cd47b478c7d83c023e8c06694acd15e9e4
khoa0319 <khoa0319@gmail.com>
2019-06-30 16:48:17 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const redis = require('redis');
+const process = require('process')
+const app = express();
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+    process.exit(0);
+    client.get('visits', (err, visits) => {
+        res.send(`Number of visits is: ${visits}`);
+        client.set('visits', parseInt(visits) + 1);
+    })
+})
+
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
